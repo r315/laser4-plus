506a2baa9547afb0ed2791dc16fdc6f6e6147f03
r315 <a38652@alunos.isel.pt>
2020-01-09 22:28:35 +0000

    Basic application for using multiprotocol

--- app/laser4_plus.cpp
+++ app/laser4_plus.cpp
@@ -18,36 +18,43 @@ void setup(void){
     usart_init();
 #endif  
     //MCO_EN;
-    #ifdef ENABLE_VCOM
+#ifdef ENABLE_VCOM
     CDC_Init();
-    #endif
+#endif
 
-    #ifdef ENABLE_GAME_CONTROLLER
+#ifdef ENABLE_GAME_CONTROLLER
     HID_Init();
     CONTROLLER_Init();
-    #endif
+#endif
 
-    #ifdef ENABLE_CONSOLE
+#ifdef ENABLE_CONSOLE
     con.init(IO_CHAR, "laser4+ >");
     con.print("\b\b\b\b\b\b\b\b\b\b");
     con.registerCommandList(laser4_commands);
-    #endif
+#endif
 
+    // Configure PPM pin input pull-up
     HW_PPM_INPUT_INIT;
+    // Attach falling edge interrupt
     BOARD_GPIO_Interrupt(GPIOB, HW_PPM_INPUT_PIN, 0, PPM_decode);
 
     NV_Init();
 
     multiprotocol_setup();
 
-    enableWatchDog(2000);
+    enableWatchDog(3000);   // 3 seconds
 }
 
 void loop(void){
-    multiprotocol_loop();
+    //multiprotocol_loop();
     #ifdef ENABLE_CONSOLE
     con.process();
     #endif
+    #ifdef ENABLE_GAME_CONTROLLER
+    CONTROLLER_Process();
+    #endif
+    multiprotocol_loop();
+    reloadWatchDog();
 }
 
 
