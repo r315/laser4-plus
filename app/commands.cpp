cfbcc028eff83025005abfed96b3d604556343a2
r315 <a38652@alunos.isel.pt>
2020-01-24 00:05:50 +0000

    ADD: info to status

--- app/commands.cpp
+++ app/commands.cpp
@@ -153,6 +153,14 @@ public:
 			IS_TX_MAIN_PAUSE_on,
 			IS_INPUT_SIGNAL_on
 		);
+
+		for(uint8_t i = 0; i < radio.ppm_chan_max; i++){
+			DBG_PRINT("CH[%u]:\t%u\n", i, radio.channel_data[i]);
+		}
+		for(uint8_t i = radio.ppm_chan_max; i < radio.ppm_chan_max + MAX_AUX_CHANNELS; i++){
+        	DBG_PRINT("CH[%u]:\t%u\n", i, radio.channel_data[i]);
+    	}
+		DBG_PRINT("\n");
         return CMD_OK;        
 	}	
 }cmdstatus;
