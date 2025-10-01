33ee89d937c249496ae858d048ab4a42b4f3f45e
r315 <a38652@alunos.isel.pt>
2025-10-01 11:42:42 +0100

    FIX: Build errors

--- target/laser4_plus/src/laser4_plus_board.c
+++ target/laser4_plus/src/laser4_plus_board.c
@@ -177,7 +177,7 @@ static void laser4Init(void)
     i2cbus.bus_num = I2C_BUS1;
     i2cbus.cfg = I2C_CFG_DMA;
 
-    if (I2C_Init(&i2cbus) == I2C_OK){
+    if (I2C_Init(&i2cbus) == I2C_SUCCESS){
         gpioInit(GPIOB, 10, GPO_MS_AF_OD);
         gpioInit(GPIOB, 11, GPO_MS_AF_OD);
     }else{
